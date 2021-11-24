b8951d02e81735d0a46520ed43a175350a014586
Hendrik <hendrik@schaefer.be>
2021-11-24 10:10:39 +0000

    mobile optimisations

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home() {
             <p className="md:text-6xl text-4xl font-bold text-left">Seconds: {seconds}</p>
           </div>
 
-          <div className="z-40 mt-8 text-2xl container mx-0">
+          <div className="z-40 mt-8 text-base md:text-2xl container mx-0">
             <form className="flex-1">
               <input className="p-2 pl-4 text-black rounded-l-2xl" id="email" type="email" aria-label="email address" placeholder="add email" value={input} onChange={e => setInput(e.target.value)} />
               <button className="bg-black hover:bg-blue-700 duration-100 text-white p-2 pr-4 pl-4 rounded-r-2xl" type='submit' onClick={subscribe}>
